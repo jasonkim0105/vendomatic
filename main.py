0ee8ad839a4fc0115960a3f11b7d1276027c5915
Jason <k.jason0105@gmail.com>
2026-02-12 13:43:31 -0600

    put inventory logic done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from dataclasses import dataclass
 from flask import Flask, request, jsonify, Response
 from typing import List
 
 app = Flask(__name__)
 
 coins_inserted: int = 0
-inventory = [5] * 3
+inventory = [5, 5, 5]
+price = 2
 
 
 @app.route("/")
@@ -52,8 +52,35 @@ def get_inventory_id(beverage_id: int):
   return jsonify(inventory[beverage_id - 1]), 200
 
 #PUT /inventory/:id
+@app.route("/inventory/<int:beverage_id>", methods=["PUT"])
+def put_inventory_purchase(beverage_id: int):
+  global coins_inserted, inventory
 
+  #return 403 if not enough coins inserted
+  if coins_inserted < price:
+    resp = Response(status=403)
+    resp.headers["X-Coins"] = "0"
+    return resp
 
+  idx = beverage_id - 1
+
+  #return 404 if inventory is at 0 of particular beverage
+  if inventory[idx] < 1:
+    resp = Response(status=404)
+    resp.headers["X-Coins"] = str(coins_inserted)
+    return resp
+
+  #return 200 if possible to buy beverage
+  can_buy = coins_inserted // price
+  beverages_vended = min(can_buy, inventory[idx])
+  change = coins_inserted - (beverages_vended * price)
+  inventory[idx] -= beverages_vended
+  coins_inserted = 0
+
+  resp = Response(status=200)
+  resp.headers["X-Coins"] = str(change)
+  resp.headers["X-Inventory-Remaining"] = str(inventory[idx])
+  return resp
 
 
 if __name__ == "__main__":
